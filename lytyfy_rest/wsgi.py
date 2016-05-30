ede2722c83a685848451bf3f8b00e907c90a9e2f
DILIP S <dilipskumar1410@gmail.com>
2016-05-30 23:18:21 +0530

    dev config

--- lytyfy_rest/wsgi.py
+++ lytyfy_rest/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "lytyfy_rest.prod")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "lytyfy_rest.dev")
 
 application = get_wsgi_application()
